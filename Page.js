e91eb62edf6e3f94432eedc29644342d532cfb82
Loh Zhen Hao <lzhlzhlzhen@gmail.com>
2024-07-08 20:13:07 +0800

    Add files via upload

---
+++ Page.js
@@ -0,0 +1,105 @@
+function home() {
+    document.getElementById("headericon").innerHTML = "HOME";
+        var A = document.getElementById("homepage");
+        var B = document.getElementById("aboutpage");
+        var C = document.getElementById("portfpage");
+        var D = document.getElementById("projectpage");
+        var E = document.getElementById("achievepage");
+        var F = document.getElementById("passionpage")
+            A.style.display = "block";
+            B.style.display = "none";
+            C.style.display = "none";
+            D.style.display = "none";
+            E.style.display = "none";
+            F.style.display = "none";
+} 
+function about() {
+    document.getElementById("headericon").innerHTML = "ABOUT ME";
+        var A = document.getElementById("homepage");
+        var B = document.getElementById("aboutpage");
+        var C = document.getElementById("portfpage");
+        var D = document.getElementById("projectpage");
+        var E = document.getElementById("achievepage");
+        var F = document.getElementById("passionpage")
+            A.style.display = "none";
+            B.style.display = "block";
+            C.style.display = "none";
+            D.style.display = "none";
+            E.style.display = "none";
+            F.style.display = "none";
+} 
+function portfolio() {
+    document.getElementById("headericon").innerHTML = "PORTFOLIO";
+        var A = document.getElementById("homepage");
+        var B = document.getElementById("aboutpage");
+        var C = document.getElementById("portfpage");
+        var D = document.getElementById("projectpage");
+        var E = document.getElementById("achievepage");
+        var F = document.getElementById("passionpage")
+            A.style.display = "none";
+            B.style.display = "none";
+            C.style.display = "block";
+            D.style.display = "none";
+            E.style.display = "none";
+            F.style.display = "none";
+} 
+function project() {
+    document.getElementById("headericon").innerHTML = "PROJECTS";
+        var A = document.getElementById("homepage");
+        var B = document.getElementById("aboutpage");
+        var C = document.getElementById("portfpage");
+        var D = document.getElementById("projectpage");
+        var E = document.getElementById("achievepage");
+        var F = document.getElementById("passionpage")
+            A.style.display = "none";
+            B.style.display = "none";
+            C.style.display = "none";
+            D.style.display = "block";
+            E.style.display = "none";
+            F.style.display = "none";
+} 
+function achieve() {
+    document.getElementById("headericon").innerHTML = "ACHIEVEMENTS";
+        var A = document.getElementById("homepage");
+        var B = document.getElementById("aboutpage");
+        var C = document.getElementById("portfpage");
+        var D = document.getElementById("projectpage");
+        var E = document.getElementById("achievepage");
+        var F = document.getElementById("passionpage")
+            A.style.display = "none";
+            B.style.display = "none";
+            C.style.display = "none";
+            D.style.display = "none";
+            E.style.display = "block";
+            F.style.display = "none";
+} 
+function passion() {
+    document.getElementById("headericon").innerHTML = "PASSION";
+        var A = document.getElementById("homepage");
+        var B = document.getElementById("aboutpage");
+        var C = document.getElementById("portfpage");
+        var D = document.getElementById("projectpage");
+        var E = document.getElementById("achievepage");
+        var F = document.getElementById("passionpage")
+            A.style.display = "none";
+            B.style.display = "none";
+            C.style.display = "none";
+            D.style.display = "none";
+            E.style.display = "none";
+            F.style.display = "block";
+} 
+function onstart() {
+    var A = document.getElementById("homepage");
+    var B = document.getElementById("aboutpage");
+    var C = document.getElementById("portfpage");
+    var D = document.getElementById("projectpage");
+    var E = document.getElementById("achievepage");
+    var F = document.getElementById("passionpage")
+        A.style.display = "block";
+        B.style.display = "none";
+        C.style.display = "none";
+        D.style.display = "none";
+        E.style.display = "none";
+        F.style.display = "none";
+}
+window.onload = onstart();
